3e5a6ede50c4554720aade75f286e7ca2306f34f
NoorHajDawood <noorhajdawood@gmail.com>
2021-12-05 01:42:44 +0200

    added messing field

--- backend/models/recipe.js
+++ backend/models/recipe.js
@@ -6,6 +6,7 @@ const recipeSchema = new Schema({
     description: { type: String, required: true },
     prepTime: { type: String, required: true },
     serving: { type: Number, required: true },
+    memberId: { type: String },
     ingredients: [{
           "name": {type: String , required: true},
           "quantity":{type: Number , required: true},
