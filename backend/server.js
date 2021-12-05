d133df6093193383e1df7d4caa577db0d8291b7d
NoorHajDawood <noorhajdawood@gmail.com>
2021-12-05 02:20:57 +0200

    corsConfig

--- backend/server.js
+++ backend/server.js
@@ -8,15 +8,15 @@ const { SESSION_SECRET } = require("./constants");
 const app = express();
 const port = process.env.PORT || 3000;
 
-// const corsConfig = function(req, res, next) {
-//     res.header('Access-Control-Allow-Origin', '*')
-//     res.header('Access-Control-Allow-Credentials', true)
-//     res.header('Access-Control-Allow-Methods', 'GET,HEAD,OPTIONS,POST,PUT')
-//     res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization')
-//     next()
-// }
+const corsConfig = function(req, res, next) {
+    res.header('Access-Control-Allow-Origin', '*')
+    res.header('Access-Control-Allow-Credentials', true)
+    res.header('Access-Control-Allow-Methods', 'GET,HEAD,OPTIONS,POST,PUT')
+    res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization')
+    next()
+}
 
-// app.use(corsConfig);
+app.use(corsConfig);
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
