ccc9688b3d7bdc3dbcdc4629a74f3d38ff054c45
NoorHajDawood <noorhajdawood@gmail.com>
2021-12-06 17:28:34 +0200

    correct error feedback on login

--- backend/controllers/sessionsController.js
+++ backend/controllers/sessionsController.js
@@ -3,7 +3,6 @@ const User = require('../models/user');
 exports.sessionsController = {
     async login(req, res) {
         let docs;
-        const userIdParam = req.params.userId;
         const { email, password } = req.body;
         if (!email || !password) {
             res.status(403).json({ error: 'Missing username or password' });
@@ -12,7 +11,7 @@ exports.sessionsController = {
         try {
             docs = await User.findOne({ email: email });
         } catch (err) {
-            res.status(500).json({ error: `Error get User: ${userIdParam} : ${err}` });
+            res.status(500).json({ error: `Error get User: ${email} : ${err}` });
             return;
         }
         if (docs) {
@@ -28,15 +27,14 @@ exports.sessionsController = {
 
             try {
                 await User.updateOne({ _id: docs.id }, { session: docs.session });
-                // res.json(docs);
                 res.redirect(`/api/users/${docs.id}`)
             } catch (err) {
-                res.status(500).json({ error: `Error updating session for User ${req.params.userId} : ${err}` });
+                res.status(500).json({ error: `Error updating session for User ${docs.id} : ${err}` });
                 return;
             }
         }
         else {
-            res.status(404).json({ error: `User with id : ${userIdParam} not found` });
+            res.status(404).json({ error: `User with email : ${email} not found` });
         }
     },
     async logout(req, res) {
