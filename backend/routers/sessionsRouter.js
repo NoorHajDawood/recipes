6b15223a57d42580034479ca7cc570873b49c532
NoorHajDawood <noorhajdawood@gmail.com>
2021-12-06 18:27:26 +0200

    cors instead of corsConfig

--- backend/routers/sessionsRouter.js
+++ backend/routers/sessionsRouter.js
@@ -3,7 +3,7 @@ const { sessionsController } = require('../controllers/sessionsController');
 
 const sessionsRouter = new Router();
 
-sessionsRouter.get('/login', sessionsController.login);
+sessionsRouter.post('/login', sessionsController.login);
 sessionsRouter.get('/logout', sessionsController.logout);
 
 module.exports = { sessionsRouter };
--- backend/server.js
+++ backend/server.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const session = require('express-session');
+const cors = require('cors');
 const { sessionsRouter } = require("./routers/sessionsRouter");
 const { recipesRouter } = require("./routers/recipesRouter");
 const { usersRouter } = require("./routers/usersRouter");
@@ -8,15 +9,16 @@ const { SESSION_SECRET } = require("./constants");
 const app = express();
 const port = process.env.PORT || 3000;
 
-const corsConfig = function(req, res, next) {
-    res.header('Access-Control-Allow-Origin', '*')
-    res.header('Access-Control-Allow-Credentials', true)
-    res.header('Access-Control-Allow-Methods', 'GET,HEAD,OPTIONS,POST,PUT,PATCH,DELETE')
-    res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization')
-    next()
-}
+app.use(cors());
+// const corsConfig = function(req, res, next) {
+//     res.header('Access-Control-Allow-Origin', '*')
+//     res.header('Access-Control-Allow-Credentials', true)
+//     res.header('Access-Control-Allow-Methods', 'GET,HEAD,OPTIONS,POST,PUT,PATCH,DELETE')
+//     res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization')
+//     next()
+// }
 
-app.use(corsConfig);
+// app.use(corsConfig);
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
