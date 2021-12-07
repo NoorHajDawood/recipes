3af4e6b6a05b08a5937b244032da9ad4850078ec
Noor Haj Dawood <noorhajdawood@gmail.com>
2021-12-07 17:24:13 +0200

    email login case insensitive

--- backend/controllers/sessionsController.js
+++ backend/controllers/sessionsController.js
@@ -13,7 +13,7 @@ exports.sessionsController = {
             return;
         }
         try {
-            user = await User.findOne({ email: new RegExp(email, 'i') });
+            user = await User.findOne({ email: new RegExp('^' + email + '$', 'i') });
         } catch (err) {
             res.status(500).json({ error: `Error get User: ${email} : ${err}` });
             return;
